904e84790996b8059049d463c9b9aab6dae4c2b4
D_Garbar <DimaGarbar49@gmail.com>
2019-04-03 09:47:19 +0300

    Finish dbInitializer

--- account-db-initializer/src/main/java/ua/procamp/AccountDbInitializer.java
+++ account-db-initializer/src/main/java/ua/procamp/AccountDbInitializer.java
@@ -33,7 +33,17 @@ public class AccountDbInitializer {
   public void init() throws SQLException {
     try (Connection connection = dataSource.getConnection()) {
       Statement statement = connection.createStatement();
-      String sql = "CREATE TABLE account(id BIGINT NOT NULL , email VARCHAR(255) NOT NULL,  first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, gender VARCHAR(255) NOT NULL, birthday DATE NOT NULL, balance DECIMAL (19,4), creation_time TIMESTAMP NOT NULL DEFAULT now(),CONSTRAINT account_pk PRIMARY KEY (id) , CONSTRAINT account_email_uq UNIQUE (email))";
+      String sql = "CREATE TABLE account("
+          + "id BIGINT NOT NULL,"
+          + "email VARCHAR(255) NOT NULL,"
+          + "first_name VARCHAR(255) NOT NULL,"
+          + "last_name VARCHAR(255) NOT NULL,"
+          + "gender VARCHAR(255) NOT NULL,"
+          + "birthday DATE NOT NULL,"
+          + "balance DECIMAL (19,4),"
+          + "creation_time TIMESTAMP NOT NULL DEFAULT now(),"
+          + "CONSTRAINT account_pk PRIMARY KEY (id),"
+          + "CONSTRAINT account_email_uq UNIQUE (email))";
       boolean execute = statement.execute(sql);
 
     }
